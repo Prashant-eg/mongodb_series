0e70d88a7ef75812d626cdcecaf2e46aa15fd7b2
Prashant Shukla <ps813870@gmail.com>
2026-06-11 11:34:23 +0530

    Update 04_delete.mongodb.js

--- 04_delete.mongodb.js
+++ 04_delete.mongodb.js
@@ -1,6 +1,6 @@
 use ('ecommerce')
  // deleteOne() - deletes a single document that matches the specified filter criteria. If multiple documents match, only the first one will be deleted. If no documents match, no deletion will occur.
-// db.contacts.deleteOne({ name: "Alice" })
+db.contacts.deleteOne({ name: "Alice" })
 
 // deleteMany() - deletes multiple documents that match the specified filter criteria. All documents that match the filter will be deleted. If no documents match, no deletion will occur.
 db.orders.deleteMany({ status: "Delivered" })
