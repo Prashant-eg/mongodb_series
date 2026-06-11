9c5277a3c5e9cdc5d61ed8adb6fda28c2dea9c05
Prashant Shukla <ps813870@gmail.com>
2026-06-11 11:34:46 +0530

    Update 05_index.mongodb.js

--- 05_index.mongodb.js
+++ 05_index.mongodb.js
@@ -4,12 +4,12 @@ db.products.createIndex({ name: 1 })
 
 
 // The above command creates an index on the "name" field in ascending order (1). You can also create a descending index by using -1 instead of 1. For example, to create a descending index on the "price" field, you would use:
-// db.products.getIndexes()
+db.products.getIndexes()
 
 //ascending index on name field
-// db.products.createIndex({ name: 1 })
+ db.products.createIndex({ name: 1 })
   //list all indexes on products collection
-// db.products.getIndexes()
+db.products.getIndexes()
 
 
 db.products.find({ price: { $gt: 5000 } }).explain("executionStats")
